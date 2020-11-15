da823ff3567dfd05be3c73a89a94e456040a05b5
Jen Hoskins <jen.c.hoskins@gmail.com>
2020-11-15 18:39:49 +0000

    Remove recharts

--- components/tree-chart.tsx
+++ components/tree-chart.tsx
@@ -1,6 +1,5 @@
 import { Component, Fragment } from 'react'
 import { getTrees } from '../src/ecologi-api/trees'
-import { Line, LineChart } from 'recharts';
 
 class TreeChart extends Component {
   constructor(props) {
@@ -10,13 +9,9 @@ class TreeChart extends Component {
 
   componentDidMount() {
     getTrees()
-      .then(res => res.data.data)
-      .then(data => {
-        console.log('Loaded!', data[0])
-        return data
-      })
-      .then(data => this.setState({ loading: false, data }))
-      .catch(err => console.error(err))
+      .then((res) => res.data.data)
+      .then((data) => this.setState({ loading: false, data }))
+      .catch((err) => console.error(err))
   }
 
   render() {
@@ -24,11 +19,7 @@ class TreeChart extends Component {
 
     return (
       <Fragment>
-        { loading ? 'Loading...' :
-          <LineChart width={400} height={400} data={[data[0], data[1], data[2]]}>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-          </LineChart>
-        }
+        {loading ? 'Loading...' : `Chart goes here: ${data[0].toString()}`}
       </Fragment>
     )
   }

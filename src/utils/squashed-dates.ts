2bf039a22b6e04c049fc6643617ab3fab0fa9585
Jen Hoskins <jen.c.hoskins@gmail.com>
2020-11-16 18:41:39 +0000

    Type squashedDates output

--- src/utils/squashed-dates.ts
+++ src/utils/squashed-dates.ts
@@ -1,6 +1,8 @@
 import { TreePlanting } from '../ecologi-api/trees'
 
-const squashedDates = (data: Array<TreePlanting>): Array<any> =>
+const squashedDates = (
+  data: Array<TreePlanting>
+): Array<{ value: number; createdAt: Date }> =>
   data.map((obj) => ({
     value: obj.value,
     createdAt: new Date(obj.createdAt.substring(0, 10)),
